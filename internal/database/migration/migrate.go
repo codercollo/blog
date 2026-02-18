de472f74bbf87eb179c7b883ee1d4df0eb1c821a
codercollo <itscollinsmaina@gmail.com>
2026-02-18 21:43:51 +0300

    Corrected the database migration configuration

--- internal/database/migration/migrate.go
+++ internal/database/migration/migrate.go
@@ -4,21 +4,17 @@ import (
 	"fmt"
 	"log"
 
-	articleMmodels "github.com/codercollo/blog/internal/modules/article/models"
+	articleModels "github.com/codercollo/blog/internal/modules/article/models"
 	userModels "github.com/codercollo/blog/internal/modules/user/models"
 	"github.com/codercollo/blog/pkg/database"
 )
 
 func Migrate() {
 	db := database.Connection()
-
-	err := db.AutoMigrate(&userModels.User{}, &articleMmodels.Article{})
-
+	err := db.AutoMigrate(&userModels.User{}, &articleModels.Article{})
 	if err != nil {
 		log.Fatal("Cannot migrate")
 		return
 	}
-
 	fmt.Println("Migration done!")
-
 }
